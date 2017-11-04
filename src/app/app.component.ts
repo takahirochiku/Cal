3570318e7d587b8ed5f9da43bba920745933d61c
takahirochiku <tik.chiku@gmail.com>
2017-11-05 01:19:26 +0900

    app.componentのhtml,tsを実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  initValue: number;
+  rate: number;
+
+  //複利計算
+  calc(): number {
+    if (isNaN(this.initValue) || isNaN(this.rate)) {
+      //元本もしくは利率が数字ではない場合nullを返す
+      return null;
+    }
+    let answer: number = this.initValue;
+    for (let i = 0; i < 10; i++) {//金利計算を10回繰り返す
+      answer = answer * (1 + this.rate / 100);
+    }
+    //整数に並び変える
+    return Math.floor(answer);
+  }
+
 }
